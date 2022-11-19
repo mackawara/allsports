2ea45464e9fa83247fe1785aea9fd2d51432972d
mackawara <mkawara@outlook.com>
2022-11-19 14:41:56 +0200

    feat: added maerial ui and fontawesone

--- allsportsfe/src/App.js
+++ allsportsfe/src/App.js
@@ -3,20 +3,8 @@ import React from "react";
 import FixtureCard from "./components/FixtureCard";
 import { Container } from "@mui/material";
 
-
-
-
 function App() {
-  const [data, setData] = React.useState(null);
-  React.useEffect(() => {
-    fetch("/getScores")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.message);
-        console.log(data);
-      });
-  }, []);
-
+  
   return (
     <Container>
       <FixtureCard></FixtureCard>
