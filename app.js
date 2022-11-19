f2fca5c46804d4dd2d29cff9dcfde9de6931f36a
mackawara <mkawara@outlook.com>
2022-11-19 14:40:42 +0200

    feat : change app configuratons

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
   });
  */
 module.exports = connectDB;
-const sendWhatsapp=require("./config/sendWhatsapp")
+const sendWhatsapp = require("./config/sendWhatsapp");
 
 const axios = require("axios");
 let date = new Date(),
@@ -77,23 +77,40 @@ axios.request(options).then(function (response) {
 app.get("/api", (req, res) => {
   console.log("pinged by react");
   res.json({ message: "Hello from server!" });
-}); 
+});
 app.get("/getScores", async (req, res) => {
- 
+  console.log("get scores pinged");
   const scores = await axios
     .request(options)
     .then((response) => {
-     data = response.data;
-      console.log(data.response);
-      // console.log("scores"+scores)
-      return data;
+      data = response.data;
+     // console.log(data.response);
+      return data.response;
     })
     .catch(function (error) {
       console.error(error);
-    })
-
-  res.send(scores);
-  const fixtures1=scores.response[0]
+    }); 
+    console.log(scores[0].fixture.status.elapsed)
+    //scores array of objects for each fixture
+    /* 
+    key details
+    fixture:{
+      TIme in local
+    Status}
+    league:{
+      name
+      season
+    }
+    teams:{
+      home.name
+      away.name
+    }
+    goals:`${home.name} ${goals.home} ${goals.away} ${away.name} 
+    minuutes played :fixture.status.elapsed
+    */
+  res.send({ body: "test" });
+  //res.send(JSON.stringify(scores));
+  /* const fixtures1=scores.response[0]
   console.log(fixtures1)
   const home = fixture1.teams.home,
   away = fixture1.teams.away,
@@ -103,121 +120,9 @@ app.get("/getScores", async (req, res) => {
   venue = fixture1.fixture.venue.name,
   time = new Date(fixture1.fixture.timestamp * 1000).toLocaleTimeString();
   const message=`${home.name} vs${away.name} \n ${venue} \n Time: ${time}, status`
-  
-
+   */
 });
-
-const responseObj = {
-  get: "fixtures",
-  parameters: { league: "39", date: "2022-11-13", season: "2022" },
-  errors: [],
-  results: 2,
-  paging: { current: 1, total: 1 },
-  response: [
-    {
-      fixture: {
-        id: 868097,
-        referee: "C. Kavanagh",
-        timezone: "UTC",
-        date: "2022-11-13T14:00:00+00:00",
-        timestamp: 1668348000,
-        periods: { first: null, second: null },
-        venue: {
-          id: 508,
-          name: "The American Express Community Stadium",
-          city: "Falmer, East Sussex",
-        },
-        status: { long: "Not Started", short: "NS", elapsed: null },
-      },
-      league: {
-        id: 39,
-        name: "Premier League",
-        country: "England",
-        logo: "https://media.api-sports.io/football/leagues/39.png",
-        flag: "https://media.api-sports.io/flags/gb.svg",
-        season: 2022,
-        round: "Regular Season - 16",
-      },
-      teams: {
-        home: {
-          id: 51,
-          name: "Brighton",
-          logo: "https://media.api-sports.io/football/teams/51.png",
-          winner: null,
-        },
-        away: {
-          id: 66,
-          name: "Aston Villa",
-          logo: "https://media.api-sports.io/football/teams/66.png",
-          winner: null,
-        },
-      },
-      goals: { home: null, away: null },
-      score: {
-        halftime: { home: null, away: null },
-        fulltime: { home: null, away: null },
-        extratime: { home: null, away: null },
-        penalty: { home: null, away: null },
-      },
-    },
-    {
-      fixture: {
-        id: 868098,
-        referee: "P. Tierney",
-        timezone: "UTC",
-        date: "2022-11-13T16:30:00+00:00",
-        timestamp: 1668357000,
-        periods: { first: null, second: null },
-        venue: { id: 535, name: "Craven Cottage", city: "London" },
-        status: { long: "Not Started", short: "NS", elapsed: null },
-      },
-      league: {
-        id: 39,
-        name: "Premier League",
-        country: "England",
-        logo: "https://media.api-sports.io/football/leagues/39.png",
-        flag: "https://media.api-sports.io/flags/gb.svg",
-        season: 2022,
-        round: "Regular Season - 16",
-      },
-      teams: {
-        home: {
-          id: 36,
-          name: "Fulham",
-          logo: "https://media.api-sports.io/football/teams/36.png",
-          winner: null,
-        },
-        away: {
-          id: 33,
-          name: "Manchester United",
-          logo: "https://media.api-sports.io/football/teams/33.png",
-          winner: null,
-        },
-      },
-      goals: { home: null, away: null },
-      score: {
-        halftime: { home: null, away: null },
-        fulltime: { home: null, away: null },
-        extratime: { home: null, away: null },
-        penalty: { home: null, away: null },
-      },
-    },
-  ],
-};
-const fixture1 = responseObj.response[1];
-
-const home = fixture1.teams.home,
-  away = fixture1.teams.away,
-  awayLogo=away.logo,
-  homeLogo=home.logo,
-  status=fixture1.fixture.status.long
-  venue = fixture1.fixture.venue.name,
-  time = new Date(fixture1.fixture.timestamp * 1000).toLocaleTimeString();
-
-//console.log(`${venue} at ${time}`);
-//console.log(awayLogo)
-//console.log(`${home.name} vs ${away.name} Status: ${status}`);
-sendWhatsapp("263775231426","test")
+//sendWhatsapp(263775231426,"hesi")
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
