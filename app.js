d79d14f4110d19489292c378b10595a47e5523ab
mackawara <mkawara@outlook.com>
2022-11-20 13:17:36 +0200

    feat: add moment for date formatting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const moment = require("moment");
 //const routes = require('./routes');
 require("dotenv").config();
 
@@ -13,26 +14,17 @@ const connectDB = require("./config/database");
 
 //routes(app);
 const PORT = process.env.PORT || 3001;
-/* connectDB().then(() => {
-  
-  });
- */
+
 module.exports = connectDB;
 const sendWhatsapp = require("./config/sendWhatsapp");
 
 const axios = require("axios");
-let date = new Date(),
-  today = date
-    .toLocaleString()
-    .split(",")[0]
-    .replace("/", "-")
-    .replace("/", "-");
-console.log(today);
+
 const rapidApiKey = process.env.X_RAPID_API_KEY;
 const options = {
   method: "GET",
   url: "https://api-football-v1.p.rapidapi.com/v3/fixtures",
-  params: { league: "39", season: "2022", date: "2022-11-13" },
+  params: { league: "1", season: "2022", round: "Group Stage - 1" },
   headers: {
     "X-RapidAPI-Key": rapidApiKey,
     "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
@@ -48,21 +40,34 @@ const options2 = {
     5632068726880896 +
     "/accounts?access_token=EAAJwHbsiZA7sBAOs1qrpPmDZB0GdyeMyXJEVVYVe4ZCpQYdCINq30NzmGSIqBt7OZAkg09CqplOWKnXwxxI9abBR8iXI7jOMjN3gM6J6LMPmZCUTowUN0vgpvIjWLnWc6Wyv9EqSEzlzm3BAZCt4bZBohVNebljKpKH9vEb6IFFSxErSbUGpvJXMwbXyaIByZCuIH7dZCYTHSVZCKlZBPeqvzvP2FBO3mTaOzuOr2JnHsfdaq0ZAvOgJFGxcWfQkQEdXhwEZD",
 };
+const optionsGetLeague = {
+  method: "GET",
+  url: "https://api-football-v1.p.rapidapi.com/v3/leagues",
+  params: { country: "world", season: "2022" },
+  headers: {
+    "X-RapidAPI-Key": rapidApiKey,
+    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+  },
+  /* params: { live: "all", league: "39" }, */
+};
+app.get("/getleagues", async (req, res) => {
+  const leagues = await axios
+    .request(optionsGetLeague)
+    .then((response) => {
+      data = response.data;
+      // console.log(data.response);
+      return data.response;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  res.send(leagues);
+});
 
 //today=`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
 let pageToken, tasks;
-/* axios
-  .request(options2)
-  .then(function (response) {
-    pageToken = JSON.stringify(response.data.data[0].access_token);
-    tasks = JSON.stringify(response.data.data[0].tasks);
-    console.log(pageToken);
-    console.log(tasks);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
- */
+
 /* 
 axios.request(options).then(function (response) {
   console.log(response)
@@ -74,25 +79,29 @@ axios.request(options).then(function (response) {
 	console.error(error);
 }); 
  */
+var date = moment();
+var currentDate = date.format("YYYY-MM-D");
+
+console.log(currentDate);
 app.get("/api", (req, res) => {
   console.log("pinged by react");
   res.json({ message: "Hello from server!" });
 });
 app.get("/getScores", async (req, res) => {
   console.log("get scores pinged");
-  const scores = await axios
+  const fixtures = await axios
     .request(options)
     .then((response) => {
+      console.log(response.data);
       data = response.data;
-     // console.log(data.response);
+      console.log(data.response);
       return data.response;
     })
     .catch(function (error) {
       console.error(error);
-    }); 
-    console.log(scores[0].fixture.status.elapsed)
-    //scores array of objects for each fixture
-    /* 
+    });
+
+  /* 
     key details
     fixture:{
       TIme in local
@@ -108,19 +117,35 @@ app.get("/getScores", async (req, res) => {
     goals:`${home.name} ${goals.home} ${goals.away} ${away.name} 
     minuutes played :fixture.status.elapsed
     */
-  res.send({ body: "test" });
-  //res.send(JSON.stringify(scores));
-  /* const fixtures1=scores.response[0]
-  console.log(fixtures1)
-  const home = fixture1.teams.home,
-  away = fixture1.teams.away,
-  awayLogo=away.logo,
-  homeLogo=home.logo,
-  status=fixture1.fixture.status.long,
-  venue = fixture1.fixture.venue.name,
-  time = new Date(fixture1.fixture.timestamp * 1000).toLocaleTimeString();
-  const message=`${home.name} vs${away.name} \n ${venue} \n Time: ${time}, status`
-   */
+  const fixture = fixtures[0];
+  const time = new Date(fixture.fixture.timestamp * 1000).toLocaleTimeString();
+  const date = new Date(fixture.fixture.date).toLocaleDateString();
+  const venue = fixture.fixture.venue.name;
+  const home = fixture.teams.home.name;
+  const away = fixture.teams.away.name;
+  const competition = `${fixture.league.name} ${fixture.league.season}`;
+  const goals = fixture.goals;
+  const matchStatus = fixture.fixture.status.long;
+  const scores = `${home} vs ${away}`;
+  console.log(scores);
+  const fixture1 = {
+    Competition: competition, //`${fixture.league.name} ${fixture.league.season}`,
+    venue: venue, //fixture.fixture.venue.name,
+    matchStatus: matchStatus, //fixture.fixture.status.long,
+    time: time, //new Date(fixture.fixture.timestamp * 1000).toLocaleTimeString(),
+    home: home, ///fixture.teams.home.name,
+    away: away,
+    scores: scores, //fixture.teams.away.name,
+    date: date,
+    message: ``,
+    homeLogo: fixture.teams.home.logo,
+    // score:fixture.score.fulltime
+  };
+  const messageBody = `${competition} \n${date} ${time} \n${scores} \n${venue}`;
+  console.log(messageBody);
+  
+  res.send({ body: fixture1 });
+  
 });
 //sendWhatsapp(263775231426,"hesi")
 app.listen(PORT, () => {
