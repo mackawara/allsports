2da8074a3d08cd1504057b4d161832026aa1a9fa
mackawara <mkawara@outlook.com>
2022-11-21 21:51:51 +0200

    feat : added model

---
+++ models/fixture.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const fixtureSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  venue: {
+    type: String,
+    required: true,
+  },
+  home: {
+    type: String,
+    required: true,
+  },
+  away: {
+    type: String,
+    required: true,
+  },
+  competition: {
+    type: String,
+    required: true,
+  },
+  goals: {
+    type: String,
+    required: true,
+  },
+  matchStatus: {
+    type: String,
+    required: true,
+  },
+  scores: {
+    type: String,
+    required: true,
+  },
+  fixtureId: {
+    type: String,
+    required: true,
+  },
+});
+
+const fixtureModel = mongoose.model("fixture", fixtureSchema);
+
+const saveFixture=async((req,res,next)=>{
+const fixture=new fixtureModel({
+    
+})
+})
