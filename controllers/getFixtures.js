b53a6244d29f9abe0f2e3b550b56e66c056e4213
mackawara <mkawara@outlook.com>
2022-12-19 19:33:13 +0200

    feat: added new functions

--- controllers/getFixtures.js
+++ controllers/getFixtures.js
@@ -1,11 +1,38 @@
-const fixtureModel=require("../models/fixture")
-const moment=require("moment")
+const FixtureModel = require("../models/fixture");
+const moment = require("moment");
 var date = moment();
-var currentDate = date.format("YYYY-MM-D")
+const englishPremier = new RegExp("epl", "i");
+const laLiga = new RegExp("La liga", "i");
+const serieA = new RegExp("Serie A", "i");
+const zpsl = new RegExp("ZPSL", "i");
 
-const getFixtures = async function (competition) {
-    const fixturesThisDay=await fixtureModel.find({date:date,competition:competition}).exec().catch(err=>console.log(err))
-  return fixturesThisDay.length>0?{fixtures:fixturesThisDay}:{fixtures:`${competition} has no fixtures lined up for today`}
-  
-}
-module.export=getFixtures
+const getFixtures = async(competition)=> {
+  let league;
+  if (englishPremier.test(competition)) {
+    league = 39;
+  } else if (serieA.test(competition)) {
+    league = 135;
+  } else if (laLiga.test(competition)) {
+    league = 140;
+  } else if (zpsl.test(competition)) {
+    league = 401;
+  }
+  const message = [];
+  const fixtures = await FixtureModel.find({
+    matchStatus: "Not Started",
+    leagueId: league,
+  }).exec();
+  let comp, round;
+  fixtures.forEach((fixture) => {
+    const line = `${fixture.date} *${fixture.home} vs${fixture.away}* ${fixture.time} \n`;
+    comp = fixture.competition;
+    round = fixture.round;
+    message.push(line);
+  });
+  const formatted = `AllSports Update :
+  \n ${comp}\n round:${round} \n ${message.join(" ")}
+\n  ,*To recieve league fixtures*, Send *league: name of league*  to +263715248036 as shown below  \n For English premier league fixtures, send: *league: Epl* \n For La liga send *league: La liga* etc. \n Brought to you by All sports`;
+
+  return formatted;
+};
+module.exports = getFixtures;
