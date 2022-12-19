7bddad840c952da2c5b99b132a87dce934fa5fd9
mackawara <mkawara@outlook.com>
2022-12-19 19:34:40 +0200

    feat: added file to handle incoming mesages

--- middleware/incomingWaMsg.js
+++ middleware/incomingWaMsg.js
@@ -1,86 +1,91 @@
-const searchDb=require("../controllers/searchDB")
+const searchDb = require("../controllers/searchDB");
+const sendWhatsappRep = require("../config/sendWhatsappRep");
+const getFixtures=require("../controllers/getFixtures")
+//Whatsapp tokens
+const token = process.env.ALL_SPORT_PAGE_ACCESS_TOKEN;
+const phoneID = process.env.WHATSAPP_PHONE_ID;
+
 //Models
-const MessageIdModel=require("../models/webhook")
-const ClientNumberModel=require("../models/clientNumber")
+const MessageIdModel = require("../models/webhook");
+const ClientNumberModel = require("../models/clientNumber");
+const FixtureModel = require("../models/fixture");
+
 // Regex
 const stop = new RegExp("stop", "i");
 const start = new RegExp("start", "i");
+const englishPremier = new RegExp("epl", "i");
 // Utility variabbles
 var todayDate = new Date().toISOString().slice(0, 10);
-const incomingWaMsg=async(req,res)=>{
+const incomingWaMsg = async (req, res) => {
+  //console.log(JSON.stringify(req.body, null, 2));
+  if (req.body.object) {
+    console.log("testing");
+    //check if the pyload has all the fields required
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      const messageId = req.body.entry[0].changes[0].value.messages[0].id;
+      // Achieve Idempotency  by checking if the message has already been handled
+      if (!(await searchDb("messageId", messageId, MessageIdModel))) {
+        await MessageIdModel.create({
+          date: todayDate,
+          messageId: messageId,
+          time: new Date().toLocaleTimeString(),
+        }).catch((err) => console.log(err));
+        console.log("processing message");
 
-    console.log("top hit");
-    //console.log(JSON.stringify(req.body, null, 2));
-    if (req.body.object) {
-      console.log("testing")
-      //check if the pyload has all the fields required
-      if (
-        req.body.entry &&
-        req.body.entry[0].changes &&
-        req.body.entry[0].changes[0] &&
-        req.body.entry[0].changes[0].value.messages &&
-        req.body.entry[0].changes[0].value.messages[0]
-      ) {
-        const messageId = req.body.entry[0].changes[0].value.messages[0].id;
-
-        if (
-          (await searchDb("messageId", messageId, MessageIdModel)) == false
-        ) {
-    
-
-          await MessageIdModel.create({
+        if (!(await searchDb("number", from, ClientNumberModel))) {
+          //check if the number is existing and if not save it .
+          console.log("number not found");
+          const newNumber = new ClientNumberModel({
+            number: from,
+            optedIn: true,
             date: todayDate,
-            messageId: messageId,
-            time:new Date().toLocaleTimeString()
-          }).catch((err) => console.log(err));
-          console.log("processing message");
+            phone_number_id: phone_number_id,
+            preference: "football",
+          });
+          newNumber.save();
+        }
+        // if the number is present check if any of the opt in or opt out key words
+        if (start.test(msg_body)) {
+          console.log("message with start");
+          let number = await ClientNumberModel.findOne({
+            number: from,
+          }).exec();
 
-          let msg_body =
-            req.body.entry[0].changes[0].value.messages[0].text.body;
-          let from = req.body.entry[0].changes[0].value.messages[0].from;
-          let phone_number_id =
-            req.body.entry[0].changes[0].value.metadata.phone_number_id;
-         
-          if (!(await searchDb("number", from, ClientNumberModel))) {
-            //check if the number is existing and if not save it .
-            console.log("number not found");
-            const newNumber = new ClientNumberModel({
-              number: from,
-              optedIn: true,
-              date: todayDate,
-              phone_number_id: phone_number_id,
-              preference: "football",
-            });
-            newNumber.save();
-          } else {
-            // if the number is present check if any of the opt in or opt out key words
-            if (start.test(msg_body)) {
-              console.log("message with start");
-              let number = await ClientNumberModel
-                .findOne({ number: from })
-                .exec();
-            
-              number.optedIn = true;
+          number.optedIn = true;
 
-              await number.save().then((data) => console.log(data));
-            } else if (stop.test(msg_body)) {
-              let number = await ClientNumberModel.findOne({
-                number: from,
-              });
-              number.optedIn = false;
-              await number.save().then((data) => console.log(data));
-            } else {
-            }
-          }
-
-          //}
-          res.sendStatus(200).end();
+          await number.save().then((data) => console.log(data));
+          sendWhatsappRep(from, msg_body);
+        } else if (stop.test(msg_body)) {
+          let number = await ClientNumberModel.findOne({
+            number: from,
+          });
+          number.optedIn = false;
+          await number.save().then((data) => console.log(data));
+        } else if (msg_body.includes("league")) {
+            console.log("league")
+            const lastLetter=msg_body.length
+            const league=msg_body.slice(8,lastLetter)
+          console.log(league)
+          const formatted= await getFixtures(league)
+          sendWhatsappRep(from,formatted)}
         } else {
-          res.sendStatus(404);
         }
       }
-    } else return;
-  
-}
 
-module.exports=incomingWaMsg
+      //}
+      res.sendStatus(200).end();
+    } else {
+      res.sendStatus(404);
+    }
+  } 
+module.exports = incomingWaMsg;
